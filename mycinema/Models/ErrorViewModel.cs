2375dc2df7c4d2294f69273620405862372af457
muhammad-tayyab67 <mt6780175@gmail.com>
2022-01-27 23:08:00 +0500

    Add project files.

---
+++ mycinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mycinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
